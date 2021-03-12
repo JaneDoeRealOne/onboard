7cdafbbed8716eb2443b79cc773ab528eaa13ec7
Carmen Petrea <carmen.petrea@student.kyh.se>
2021-03-12 16:20:49 +0100

    added content landing page

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -6,8 +6,21 @@ function Landing() {
     <div className='hero-container'>
       {/* <div className='text-container'> */}
       <h1 className='hero-heading'>Välkommen!</h1>
-      <h2>Du har blivit antagen till en utbildning på yrkeshögskolan KYH</h2>    
+      <h2>Du har blivit antagen till en utbildning på yrkeshögskolan KYH</h2>
+      {/* </div> */}
+      <div className='hero-btns'>
+        <a href='https://www.youtube.com/embed/euiGHk8dIPI' target="_blank" rel="noopener noreferrer" >
+          <button
+            className='btns movie--btn'
+            buttonstyle='btn--primary'
+            buttonsize='btn--large'
+          >
+            SE VIDEO <i className='far fa-play-circle' />
+          </button>
+        </a>
+      </div>
     </div>
+    
   );
 }
 
