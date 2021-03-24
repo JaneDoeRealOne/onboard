5786619aeabc15d1dbc539e202e235f73647ccd1
bobish0 <kristian.a.boban@gmail.com>
2021-03-24 11:14:40 +0100

    Added News-Carousel

---
+++ src/components/News/Item.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+ 
+export default styled.div`
+  font-family: 'Roboto', sans-serif;
+  display: flex;
+  justify-content: center;
+  flex-direction:column;
+  align-items: center;
+  height: 400px;
+  width: 100%;
+  margin: 15px;
+  border-radius: 5px;
+  border: 2px solid grey;
+`;
+
+
+ 
